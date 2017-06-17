c0d640ebe15a5f0ff7713e3bfe7ad7522e383623
Sudheer Herle <ssherle89@gmail.com>
2017-06-17 14:12:32 +0530

    AWS Shadow for my app
    The aws shadow example worked perfectly fine for my web and android
    application at home.

--- fw/examples/c_aws_shadow/src/main.c
+++ fw/examples/c_aws_shadow/src/main.c
@@ -19,19 +19,22 @@
 #include "common/platform.h"
 #include "frozen/frozen.h"
 #include "fw/src/mgos_app.h"
+#include "fw/src/mgos_aws_shadow.h"
 #include "fw/src/mgos_gpio.h"
-
-#include "mgos_aws_shadow.h"
 
 #if CS_PLATFORM == CS_P_ESP8266
 /*
  * On ESP-12E there is a blue LED connected to GPIO2 (aka U1TX).
  * LED is attached to Vdd, so writing 0 turns it on while 1 turns it off.
  */
-#define LED_GPIO 2
+#define LED_MOTOR 4
+#define LED_FAN 12
+#define LED_LIGHT 14
 #define LED_OFF 1
 #define LED_ON 0
-#define BUTTON_GPIO 0 /* Usually a "Flash" button. */
+#define BUTTON_MOTOR 0 /* Usually a "Flash" button. */
+#define BUTTON_FAN 2 /* PIN 19 */
+#define BUTTON_LIGHT 5 /* PIN 20 */
 #define BUTTON_PULL MGOS_GPIO_PULL_UP
 #define BUTTON_EDGE MGOS_GPIO_INT_EDGE_POS
 #elif CS_PLATFORM == CS_P_ESP32
@@ -70,21 +73,23 @@
 #error Unknown platform
 #endif
 
-static int s_foo = 0;
-static int s_bar = 0;
-static int s_led = false;
-#define STATE_FMT "{foo: %d, bar: %d, led: %B}"
+static int s_motor,new_motor = false;
+static int s_fan,new_fan = false;
+static int s_light,new_light = false;
+#define STATE_FMT "{motor: %B, fan: %B, light: %B}"
 
 static void report_state(void) {
   char buf[100];
   struct json_out out = JSON_OUT_BUF(buf, sizeof(buf));
-  json_printf(&out, STATE_FMT, s_foo, s_bar, s_led);
+  json_printf(&out, STATE_FMT, s_motor, s_fan, s_light);
   LOG(LL_INFO, ("== Reporting state: %s", buf));
-  mgos_aws_shadow_updatef(0, "{reported:" STATE_FMT "}", s_foo, s_bar, s_led);
+  mgos_aws_shadow_updatef(0, "{reported:" STATE_FMT "}", s_motor, s_fan, s_light);
 }
 
 void update_led(void) {
-  mgos_gpio_write(LED_GPIO, (s_led ? LED_ON : LED_OFF));
+  mgos_gpio_write(LED_MOTOR, (s_motor ? LED_ON : LED_OFF));
+  mgos_gpio_write(LED_FAN, (s_fan ? LED_ON : LED_OFF));
+  mgos_gpio_write(LED_LIGHT, (s_light ? LED_ON : LED_OFF));
 }
 
 static void aws_shadow_state_handler(void *arg, enum mgos_aws_shadow_event ev,
@@ -93,8 +98,7 @@ static void aws_shadow_state_handler(void *arg, enum mgos_aws_shadow_event ev,
                                      const struct mg_str desired,
                                      const struct mg_str reported_md,
                                      const struct mg_str desired_md) {
-  LOG(LL_INFO, ("== Event: %d (%s), version: %llu", ev,
-                mgos_aws_shadow_event_name(ev), version));
+  LOG(LL_INFO, ("== Event: %d (%s), version: %llu", ev, mgos_aws_shadow_event_name(ev), version));
   if (ev == MGOS_AWS_SHADOW_CONNECTED) {
     report_state();
     return;
@@ -105,15 +109,14 @@ static void aws_shadow_state_handler(void *arg, enum mgos_aws_shadow_event ev,
   }
   LOG(LL_INFO, ("Reported state: %.*s", (int) reported.len, reported.p));
   LOG(LL_INFO, ("Desired state : %.*s", (int) desired.len, desired.p));
-  LOG(LL_INFO,
-      ("Reported metadata: %.*s", (int) reported_md.len, reported_md.p));
+  LOG(LL_INFO, ("Reported metadata: %.*s", (int) reported_md.len, reported_md.p));
   LOG(LL_INFO, ("Desired metadata : %.*s", (int) desired_md.len, desired_md.p));
   /*
    * Here we extract values from previosuly reported state (if any)
    * and then override it with desired state (if present).
    */
-  json_scanf(reported.p, reported.len, STATE_FMT, &s_foo, &s_bar, &s_led);
-  json_scanf(desired.p, desired.len, STATE_FMT, &s_foo, &s_bar, &s_led);
+  json_scanf(reported.p, reported.len, STATE_FMT, &s_motor, &s_fan, &s_light);
+  json_scanf(desired.p, desired.len, STATE_FMT, &s_motor, &s_fan, &s_light);
   update_led();
   if (ev == MGOS_AWS_SHADOW_UPDATE_DELTA) {
     report_state();
@@ -122,22 +125,39 @@ static void aws_shadow_state_handler(void *arg, enum mgos_aws_shadow_event ev,
 }
 
 static void button_cb(int pin, void *arg) {
-  int new_foo = s_foo + 1;
-  int new_led = !s_led;
   /* We do not change local state here. In response to this update AWS will
    * generate a delta event which will be processed as any other. */
-  bool upd_res = mgos_aws_shadow_updatef(0, "{desired:{foo: %d, led: %B}}",
-                                         new_foo, new_led);
-  LOG(LL_INFO, ("== Click! %d, Updated: %d", new_foo, upd_res));
+  
+  if(pin==BUTTON_MOTOR){
+	  new_motor = !s_motor;
+	  LOG(LL_INFO, ("Motor state changed by button press to %B", new_motor));
+  }
+  if(pin==BUTTON_FAN){
+	  new_fan = !s_fan;
+	  LOG(LL_INFO, ("Fan state changed by button press to %B", new_fan));
+  }
+  if(pin==BUTTON_LIGHT){
+	  new_light = !s_light;
+	  LOG(LL_INFO, ("Light state changed by button press to %B", new_light));
+  }
+  mgos_aws_shadow_updatef(0, "{desired:{motor: %B, fan: %B, light: %B}}",
+                                         new_motor, new_fan, new_light);
   (void) pin;
   (void) arg;
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
-  mgos_gpio_set_mode(LED_GPIO, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_set_mode(LED_MOTOR, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_set_mode(LED_FAN, MGOS_GPIO_MODE_OUTPUT);
+  mgos_gpio_set_mode(LED_LIGHT, MGOS_GPIO_MODE_OUTPUT);
+  
   update_led();
-  mgos_gpio_set_button_handler(BUTTON_GPIO, BUTTON_PULL, BUTTON_EDGE,
+  mgos_gpio_set_button_handler(BUTTON_MOTOR, BUTTON_PULL, BUTTON_EDGE,
                                50 /* debounce_ms */, button_cb, NULL);
+  mgos_gpio_set_button_handler(BUTTON_FAN, BUTTON_PULL, BUTTON_EDGE,
+							   50 /* debounce_ms */, button_cb, NULL);
+  mgos_gpio_set_button_handler(BUTTON_LIGHT, BUTTON_PULL, BUTTON_EDGE,
+							   50 /* debounce_ms */, button_cb, NULL);
   mgos_aws_shadow_set_state_handler(aws_shadow_state_handler, NULL);
   return MGOS_APP_INIT_SUCCESS;
 }
